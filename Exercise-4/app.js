fa68c7a9b926e93b3f7f6942589c962539fc30d7
Lucas Macias <chueko@gmail.com>
2024-01-25 12:04:06 -0300

    Exercise 4 finish

--- Exercise-4/app.js
+++ Exercise-4/app.js
@@ -35,8 +35,9 @@ SELECT SEAT:
 TOTAL SEATS: 74310 (100%) - ORDER: VIP BOXES (5%) - VIP SEATS (15%) - GENERAL SEATS (80%)
 PURCHASE : IS MEMBER ? 10 TICKETS : 3 TICKETS. IF SEAT IS OCCUPIED, OFFER NEARBY SEATS AS RECOMMENDATION
 VIP BOX £1000 - VIP SEAT £500  - GENERAL £90
-CALCULATE TOTAL AND PRINT TICKET
 UPDATE AVAILABLE SEATS
+CALCULATE TOTAL AND PRINT TICKET
+
 */
 
 //login variables
@@ -58,7 +59,7 @@ let purchasedTickets = [];
 const vipBoxPrice = 1000;
 const vipSeatPrice = 500;
 const generalSeatPrice = 90;
-
+const ticketsAvailable = document.getElementById('tickets-availables');
 const ticketsContainer = document.getElementById('tickets-container');
 const isMemberContainer = document.getElementById('is-member-container');
 const isMemberSelect = document.getElementById('is-member-select');
@@ -69,6 +70,10 @@ const chooseTicketsContainer = document.getElementById(
 const chooseTicketsInput = document.getElementById('choose-tickets-input');
 const addTicketsButton = document.getElementById('add-ticket-button');
 const chooseTicketsButton = document.getElementById('choose-tickets-button');
+const summaryContainer = document.getElementById('summary-container');
+const ticketsList = document.getElementById('tickets-list');
+const totalPrice = document.getElementById('total-price');
+const summaryButton = document.getElementById('summary-button');
 
 //Funcion de fetch
 const fetchJson = async (url) => {
@@ -167,6 +172,10 @@ const toggleShowForm = (container = null, button = null, select = null) => {
     : (select.disabled = false);
 };
 
+const updateTicketsDOM = () => {
+  ticketsAvailable.innerHTML = `Available Seats: ${availableTickets.length}\n Seats sold: ${purchasedTickets.length}`;
+};
+
 const fillArray = (array) => {
   array.length = 0;
   for (let i = 1; i <= totalSeats; i++) {
@@ -188,22 +197,57 @@ const fillArray = (array) => {
 
   return array;
 };
+//Funcion que controla que el usuario no haya comprado
+const isTicketAlreadyPurchased = (ticketId) => {
+  return currentUser.tickets.some((ticket) => ticket.id === ticketId);
+};
 
+// Función para comprar boletos
 const buyTicket = (purchased, available, ticket) => {
   const selectedTicket = available.find((elem) => elem.id === ticket);
 
   if (selectedTicket) {
-    purchased.push(selectedTicket);
-    available = available.filter((elem) => elem.id !== ticket);
-    console.log(available);
-    console.log(purchased);
-    return { availableTickets: available, purchasedTickets: purchased };
+    if (!isTicketAlreadyPurchased(ticket)) {
+      purchased.push(selectedTicket);
+      available = available.filter((elem) => elem.id !== ticket);
+
+      return { availableTickets: available, purchasedTickets: purchased };
+    } else {
+      alert('That ticket was already reserved, please choose another');
+    }
   }
 
   return { availableTickets: available, purchasedTickets: purchased };
 };
-
+//Creamos el array de tickets con los 74310 asientos
 let availableTickets = fillArray([]);
+updateTicketsDOM();
+//Funcion para ofrecer asientos cercanos hasta 6 lugares en ambas direcciones.
+//Iba a poner que solo ofrezca del mismo tipo (general, vip, etc) pero no terminaba mas
+const findNearbySeats = (available, targetSeat) => {
+  const nearbySeats = [];
+  const maxDistance = 6;
+
+  for (let i = 1; i <= maxDistance; i++) {
+    const seatBefore = targetSeat - i;
+    const seatAfter = targetSeat + i;
+
+    if (seatBefore > 0 && !isTicketAlreadyPurchased(seatBefore)) {
+      nearbySeats.push(seatBefore);
+    }
+
+    if (seatAfter <= totalSeats && !isTicketAlreadyPurchased(seatAfter)) {
+      nearbySeats.push(seatAfter);
+    }
+
+    // Cortamos la ejecución cuando llegue al maximo establecido
+    if (nearbySeats.length >= maxDistance) {
+      break;
+    }
+  }
+
+  return nearbySeats;
+};
 
 //Escuchamos el boton de isMember
 isMemberButton.addEventListener('click', (e) => {
@@ -211,21 +255,22 @@ isMemberButton.addEventListener('click', (e) => {
   toggleShowForm(chooseTicketsContainer, isMemberButton, isMemberSelect);
 });
 
-// Escuchamos el click de addTickets
-
+// Escuchamos el click de addTickets.
 addTicketsButton.addEventListener('click', (e) => {
   e.preventDefault();
   const purchasedTicketIds = purchasedTickets.map((ticket) => ticket.id);
   availableTickets = availableTickets.filter(
     (ticket) => !purchasedTicketIds.includes(ticket.id)
   );
+  //Asignamos la cantidad que puede comprar el usuario
   let maxquantity;
   if (isMemberSelect.value == 'Yes') {
     maxquantity = 10;
   } else {
     maxquantity = 3;
   }
-
+  // Validamos que no pueda comprar mas de las que puede y que elija un numero dentro del rango posible,
+  // y que ese número de ticket esté disponible.
   if (count < maxquantity) {
     let ticketinputSelected = Number(chooseTicketsInput.value);
     if (
@@ -236,22 +281,64 @@ addTicketsButton.addEventListener('click', (e) => {
       alert('Please enter a valid number of ticket between 1 and 74310');
     } else {
       if (purchasedTicketIds.includes(ticketinputSelected)) {
-        alert('That ticket was reserved, please choose another');
+        //  Si el asiento no disponible, sugerir asientos cercanos
+        const nearbySeats = findNearbySeats(
+          availableTickets,
+          ticketinputSelected
+        );
+        if (nearbySeats.length > 0) {
+          alert(
+            `The selected seat is not available. You can reserve one of that nearby seats: (${nearbySeats.join(
+              ', '
+            )})`
+          );
+        }
       } else {
+        //Si está todo bien, le asignamos la compra. Actualizamos los tickets y se añade al currentUser
         const result = buyTicket(
           purchasedTickets,
           availableTickets,
           ticketinputSelected
         );
+
+        //Debería en realidad hacer que los tickets estén asociados al usuario para que si viene otro user
+        // no compre todos los tickets del usuario anterior inclusive. Pero sería mucho trabajo adicional.
+
         purchasedTickets = result.purchasedTickets;
         availableTickets = result.availableTickets;
+        currentUser.tickets.length = 0;
         currentUser.tickets.push(...result.purchasedTickets);
         chooseTicketsInput.value = '';
         chooseTicketsButton.style.display = 'block';
+        updateTicketsDOM();
         count++;
       }
     }
   } else {
     alert('You have reached the maximum quantity of tickets.');
+    addTicketsButton.style.display = 'none';
   }
+});
+
+chooseTicketsButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  addTicketsButton.style.display = 'none';
+  ticketsContainer.style.display = 'none';
+  toggleShowForm(summaryContainer, chooseTicketsButton, chooseTicketsInput);
+  ticketsList.innerHTML = '';
+  currentUser.tickets.forEach((ticket) => {
+    const li = document.createElement('li');
+    li.textContent = `Ticket ID: ${ticket.id}, Price: ${ticket.price}, Seat Type: ${ticket.seatType}`;
+    ticketsList.appendChild(li);
+  });
+  //Hacemos un reduce que nos devuelva la sumatoria de todos los prices de cada ticket.
+  const toPay = currentUser.tickets.reduce(
+    (total, ticket) => total + ticket.price,
+    0
+  );
+  totalPrice.innerHTML = `Total Price of your tickets is: ${toPay}`;
+});
+
+summaryButton.addEventListener('click', () => {
+  alert('Thanks for buy tickets. Enjoy the Game!');
 });
